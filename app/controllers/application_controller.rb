6f82d662f1b5da05497dff7cccb277cd275adea3
bjgraveline-appdev <bgraveline@chicagobooth.edu>
2023-01-25 07:58:12 +0000

    everything but payment works.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,41 @@ class ApplicationController < ActionController::Base
     render({:template => "calculation_templates/square_form.html.erb"})
   end
 
-  def blank_random_form
-    render({:template => "calculation_templates/random_form.html.erb"})
-  end
-
-
   def calculate_square
     @num = params.fetch("number").to_f
     @square_of_num = @num * @num
     render({:template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_squareroot_form
+    render({:template => "calculation_templates/squareroot_form.html.erb"})
+  end
+
+  def calculate_squareroot
+    @num = params.fetch("number").to_f
+    @squareroot_of_num = Math.sqrt(@num)
+    render({:template => "calculation_templates/squareroot_results.html.erb"})
+  end
+
+  def blank_payment_form
+    render({:template => "calculation_templates/payment_form.html.erb"})
+  end
+
+  def calculate_payment
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_i
+    @pv = params.fetch("user_pv").to_f
+    @n = @years * 12
+    @r = (@apr/100)/@n
+    @numerator = (@r * @pv)
+    @denominator = (1 - (@r+1) ** (-1 * @n))
+    #Payment = pv*apr/12*(1+apr/12)^(nper*12)/((1+apr/12)^(nper*12)-1)
+    @payment = @pv * @r*(1+@r)**@n/((1+@r)**@n-1)
+    render({:template => "calculation_templates/payment_results.html.erb"})
+  end
+
+  def blank_random_form
+    render({:template => "calculation_templates/random_form.html.erb"})
   end
 
   def calculate_random
